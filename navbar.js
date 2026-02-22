904d4ae8ebb0aac5b996b50e5c8521fefc05a942
Himanshujangra400 <Jangrahimanshu076@gmail.com>
2026-02-22 21:28:23 +0530

    code fix

--- navbar.js
+++ navbar.js
@@ -32,9 +32,10 @@ function Navbar(activePage = "") {
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"/>
                     </svg>
                 </div>
+                <a href="index.html">
                 <span class="text-2xl font-extrabold tracking-tight text-slate-800">
                     EDUVAULT
-                </span>
+                </span> </a>
             </div>
 
             <div class="hidden md:flex gap-10 font-medium">
